29bcb55475cf316e4d11d29f9c540c7e8f468bff
Dani-XT <danielte98@gmail.com>
2026-04-30 00:47:58 -0400

    pagina auth y pagina de services, modulos compartidos

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { UtilsService } from '../../services/utils';
+
+import { SharedModule } from '../../shared/shared';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './auth.html',
   styleUrl: './auth.css'
 })
 export class Auth {
+
+  utilsSvc = inject(UtilsService)
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6)])
+  })
+
+  async submit() {
+    if (this.form.valid) {
+      this.utilsSvc.routerLink("home");
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 
 }
